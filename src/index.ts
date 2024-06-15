6b63b2ecbcf72e9ca19f119fd1757a14a3b5567f
Wésley Guimarães <wes@guima.dev>
2024-06-15 00:37:35 -0300

    feat: first solution

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,37 @@
-console.log("running");
+import { inputList, type Input } from './input'
+import type { Output } from './output'
+
+const toOutput = (input: Input): Output => ({
+  entryId: Number.parseInt(input.entryId, 10),
+  currentPath: input.path.at(-1) ?? '',
+  fullPath: input.path.join('/'),
+  children: [],
+})
+
+const transformInputs = (items: Input[]): Output[] => {
+  const result: Output[] = []
+
+  for (const input of items.sort((a, b) => a.path.length - b.path.length)) {
+    const output = toOutput(input)
+
+    let parent: Output | undefined
+
+    for (const item of input.path.slice(0, -1)) {
+      if (parent === undefined) {
+        parent = result.find((r) => r.currentPath === item) ?? undefined
+      } else {
+        parent = parent.children.find((r) => r.currentPath === item) ?? undefined
+      }
+    }
+
+    if (!parent) {
+      result.push(output)
+    } else {
+      parent.children = [...parent.children, output].sort((a, b) => a.entryId - b.entryId)
+    }
+  }
+
+  return result.sort((a, b) => a.entryId - b.entryId)
+}
+
+console.log(transformInputs(inputList))
