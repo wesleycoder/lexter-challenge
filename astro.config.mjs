b172a9ceb8eee030d89f15284afa12ca815cada9
Wésley Guimarães <wes@guima.dev>
2024-06-17 20:50:54 -0300

    feat: vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import solidJs from '@astrojs/solid-js'
 import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: vercel(),
   integrations: [solidJs({ devtools: true }), tailwind({ applyBaseStyles: false })],
   experimental: {
     actions: true,
